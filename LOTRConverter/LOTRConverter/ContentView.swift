02ddc5386fe0e2d220cc97cc287ba84a6f108900
Sudha Rani <sudhatiwari22@gmail.com>
2025-05-15 20:52:08 +0530

    Added views to our stacks according to design

--- LOTRConverter/LOTRConverter/ContentView.swift
+++ LOTRConverter/LOTRConverter/ContentView.swift
@@ -13,10 +13,22 @@ struct ContentView: View {
         ZStack {
             // Background Image
             
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
             VStack {
                 // Pony Image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
                 
                 //Currency Exchange Text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
                 
                 //Conversion section
                 
@@ -28,14 +40,26 @@ struct ContentView: View {
                         
                         HStack {
                             // Currency Image
+                            Image(.silverpenny)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                             
                             // Currency Text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                         }
                         
                         //Text Field
+                        Text("Text Field")
                     }
                     
                     // Equal Sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
                     
                     // Right Conversion section
                     
@@ -44,17 +68,31 @@ struct ContentView: View {
                         
                         HStack {
                             // Currency Text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                             
                             // Currency Image
+                            Image(.goldpenny)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                         }
                         
                         //Text Field
+                        Text("Text Field")
                     }
 
                 }
                 
+                Spacer()
+                
                 //Info Button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
             }
+//            .border(.blue)
         }
     }
 }
