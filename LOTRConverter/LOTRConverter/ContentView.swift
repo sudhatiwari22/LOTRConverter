f49bd42bbb80f898169232d72f169238708d0ffa
Sudha Rani <sudhatiwari22@gmail.com>
2025-05-15 20:19:59 +0530

    Laying Out main screen with Stacks

---
+++ LOTRConverter/LOTRConverter/ContentView.swift
@@ -0,0 +1,64 @@
+//
+//  ContentView.swift
+//  LOTRConverter
+//
+//  Created by Sudha Rani on 15/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       
+        ZStack {
+            // Background Image
+            
+            VStack {
+                // Pony Image
+                
+                //Currency Exchange Text
+                
+                //Conversion section
+                
+                HStack {
+                    // Left conversion section
+                    
+                    VStack {
+                        // currency
+                        
+                        HStack {
+                            // Currency Image
+                            
+                            // Currency Text
+                        }
+                        
+                        //Text Field
+                    }
+                    
+                    // Equal Sign
+                    
+                    // Right Conversion section
+                    
+                    VStack {
+                        // currency
+                        
+                        HStack {
+                            // Currency Text
+                            
+                            // Currency Image
+                        }
+                        
+                        //Text Field
+                    }
+
+                }
+                
+                //Info Button
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter/LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Sudha Rani on 15/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
